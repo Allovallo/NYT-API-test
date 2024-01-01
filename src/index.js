b5121fd60a0d4252d94a8c4f1c7b1a323e3809aa
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-01 21:19:33 +0200

    article`s draft

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@ import {
   fetchPopularNews,
   fetchPopularNewsOnFacebook,
 } from './scripts/newsApi';
+import newsTemplate from './templates/news.hbs';
 
-fetchPopularNews().then(res => console.log(res));
+//title, url, abstarct, media[0].media-metadata[2]
+
+const newsListTemplate = newsList => {
+  return newsList.map(news => {
+    const { title, url, abstarct, media } = news;
+
+    return newsTemplate({
+      title,
+      url,
+      abstarct,
+      //   imageSrc: media[0]['media-metadata'][2],
+    });
+  });
+};
+
+fetchPopularNews().then(({ results }) => {
+  const newsList = newsListTemplate(results);
+  console.log(newsList);
+});
