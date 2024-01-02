c027c561762b0303776a88ff6ad3182977f92757
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-02 20:27:31 +0200

    newsList updated

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ import {
 } from './scripts/newsApi';
 import newsTemplate from './templates/news.hbs';
 
+const newsListRef = document.querySelector('.news-list');
+
 // title, url, abstract, media[0].media-metadata[2];
 
 const newsListTemplate = newsList => {
-  return newsList.map(news => {
-    const { title, url, abstract, media } = news;
+  return newsList
+    .map(news => {
+      const { title, url, abstract, media } = news;
 
-    return newsTemplate({
-      title,
-      url,
-      abstract,
-      // imageSrc: media[0]['media-metadata'][2],
-    });
-  });
+      return newsTemplate({
+        title,
+        url,
+        abstract,
+        // imageSrc: media[0]['media-metadata'][2].url,
+      });
+    })
+    .join('');
 };
 
 fetchPopularNews().then(({ results }) => {
-  const newsList = newsListTemplate(results);
-  console.log(newsList);
+  const newsListView = newsListTemplate(results);
+  newsListRef.innerHTML = newsListView;
+  console.log(newsListView);
 });
