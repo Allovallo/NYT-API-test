c17d7cc353720e3a717033d33966e307852d9deb
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-04 16:47:24 +0200

    search form_step 1

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import './sass/index.scss';
 import { fetchPopularNews, fetchSearchedArticles } from './scripts/newsApi';
-import newsTemplate from './templates/news.hbs';
+import NewsList from './scripts/newsList';
 
 const newsListRef = document.querySelector('.news-list');
+const newsList = new NewsList('.news-list');
 
 // title, url, abstract, media[0].media-metadata[2];
 
-const newsListTemplate = newsList => {
-  return newsList
-    .map(news => {
-      const { title, url, abstract, media } = news;
+const newsListAdapter = newsList => {
+  return newsList.map(news => {
+    const { title, url, abstract, media } = news;
 
-      return newsTemplate({
-        title,
-        url,
-        abstract,
-        // imageSrc: media[0]['media-metadata'][2].url,
-      });
-    })
-    .join('');
+    return {
+      title,
+      url,
+      abstract,
+      // imageSrc: media[0]['media-metadata'][2].url,
+    };
+  });
 };
 
 fetchPopularNews().then(({ results }) => {
-  const newsListView = newsListTemplate(results);
-  newsListRef.innerHTML = newsListView;
-  // console.log(newsListView);
+  const newsData = newsListAdapter(results);
+  newsList.render(newsData);
 });
 
 // fetchSearchedArticles('Ukraine').then(console.log);
--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,13 +1,39 @@
 import { fetchSearchedArticles } from './newsApi';
+import newsList from '../templates/newsList.hbs';
+import NewsList from './newsList';
 
+const newsList = new NewsList('.news-list');
 const searchFormRef = document.querySelector('.search');
+
+const newsListAdapter = newsList => {
+  newsList.map(news => {
+    const { title, url, abstract, multimedia } = news;
+    const baseImageSrc = 'https://static01.nyt.com/';
+    return {
+      title,
+      url,
+      abstract,
+      imageSrc: `${baseImageSrc}${multimedia[0].url}`,
+    };
+  });
+};
+
+const searchArticles = query => {
+  fetchSearchedArticles(value).then(result => {
+    const items = result.response.docs;
+    const newsData = newsListAdapter(items);
+    newsList.render(newsData);
+  });
+};
+
 const handleSubmit = event => {
   event.preventDefault();
 
   const { target: form } = event;
   const value = form.elements.search.value;
-  fetchSearchedArticles(value).then(console.log);
-  console.log(value);
+  searchArticles(value);
 };
 
 searchFormRef.addEventListener('submit', handleSubmit);
+
+fetchSearchedArticles('Poroshenko').then(console.log);
