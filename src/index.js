638af8dbeaf0339c0f67e037c832ad638ce00857
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-01-04 18:36:05 +0200

    index.js updated

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import './sass/index.scss';
 import { fetchPopularNews, fetchSearchedArticles } from './scripts/newsApi';
 import NewsList from './scripts/newsList';
 
-const newsListRef = document.querySelector('.news-list');
 const newsList = new NewsList('.news-list');
-
-// title, url, abstract, media[0].media-metadata[2];
-
 const newsListAdapter = newsList => {
   return newsList.map(news => {
     const { title, url, abstract, media } = news;
